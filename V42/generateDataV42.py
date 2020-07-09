b845bd0ddc70b759a89a47d3786a9ef3b92925db
mawadonna <walther@physik.uni-freiburg.de>
2020-07-10 00:22:40 +0200

    V42 spectral lines of He and Ne added

--- V42/generateDataV42.py
+++ V42/generateDataV42.py
@@ -15,8 +15,8 @@ np.random.seed(seed)
 # angleOfTotalReflection [degrees]
 
 alphas = np.linspace(30, 50, 5)
-betas = np.degrees(np.arcsin(np.sin(np.radians(alphas) / 1.5))) + \
-    np.random.normal(0, 0.5, len(alphas))
+#betas = np.degrees(np.arcsin(np.sin(np.radians(alphas) / 1.5))) + np.random.normal(0, 0.5, len(alphas))
+betas = (np.arcsin(np.sin(alphas* np.pi /180) / 1.5))*180/ np.pi + np.random.normal(0, 0.5, len(alphas))
 
 np.save(workingDir + "Data" + experiment + "Part1",
         np.round(np.array([alphas, betas]), 1))
@@ -26,3 +26,22 @@ angleOfTotalReflection = np.degrees(
 
 np.save(workingDir + "Data" + experiment + "angleOfTotalReflection",
         np.round(angleOfTotalReflection, 1))
+
+
+# _____________________________________________________________________________
+# Part 2: Spectroscopy
+# Skala [Skalenstriche]
+# Linien [nm]
+
+HeSkala = [5.8, 6.2, 7.2, 8.9, 9.1, 9.8, 10.7]
+HeLinien = [707, 668, 588, 501, 492, 471, 447]
+HeFarben = ["dunkelrot", "rot", "gelb", "grün", "blaugrün", "blau", "blau"]
+
+np.save(workingDir + "Data" + experiment + "Part2",
+        np.array([HeSkala, HeLinien, HeFarben]))
+
+NeSkala = [5.8, 6.4, 6.6, 6.7, 6.8, 7.0, 7.3, 8.1]
+NeFarben = ["dunkelrot", "dunkelrot", "rot", "rot", "rot", "orange", "gelb", "grün"]
+
+np.save(workingDir + "Data" + experiment + "Part3",
+        np.array([NeSkala, NeFarben]))
--- V42/generatepdfV42.py
+++ V42/generatepdfV42.py
@@ -21,5 +21,18 @@ angleOfTotalReflection = str(
 gpdf.addText("Winkel der Totalreflektion = {0}°".format(
     angleOfTotalReflection))
 gpdf.addSpacer()
+gpdf.addSpacer()
+
+gpdf.addText("Teil 2: Kalibrierung mit Spektrallinien von Neon")
+header2 = [["Skala", "Wellenlänge [nm]", "Farbe"]]
+data2 = np.load(workingDir + "DataV42Part2.npy").T
+gpdf.addTable(header2, data2)
+gpdf.addSpacer()
+gpdf.addSpacer()
+
+gpdf.addText("Teil 3: Bestimmung der Spektrallinien von Helium")
+header3 = [["Skala", "Farbe"]]
+data3 = np.load(workingDir + "DataV42Part3.npy").T
+gpdf.addTable(header3, data3)
 
 gpdf.createPDF(experiment, name)
