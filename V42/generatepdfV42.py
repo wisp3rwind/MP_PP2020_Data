5cffcd8885b158d25e651dbe159bb4524224a11b
mawadonna <walther@physik.uni-freiburg.de>
2020-07-18 18:55:56 +0200

    corrected: Helium and Neon swapped

--- V42/generatepdfV42.py
+++ V42/generatepdfV42.py
@@ -23,14 +23,14 @@ gpdf.addText("Winkel der Totalreflektion = {0}°".format(
 gpdf.addSpacer()
 gpdf.addSpacer()
 
-gpdf.addText("Teil 2: Kalibrierung mit Spektrallinien von Neon")
+gpdf.addText("Teil 2: Kalibrierung mit Spektrallinien von Helium")
 header2 = [["Skala", "Wellenlänge [nm]", "Farbe"]]
 data2 = np.load(workingDir + "DataV42Part2.npy").T
 gpdf.addTable(header2, data2)
 gpdf.addSpacer()
 gpdf.addSpacer()
 
-gpdf.addText("Teil 3: Bestimmung der Spektrallinien von Helium")
+gpdf.addText("Teil 3: Bestimmung der Spektrallinien von Neon")
 header3 = [["Skala", "Farbe"]]
 data3 = np.load(workingDir + "DataV42Part3.npy").T
 gpdf.addTable(header3, data3)
